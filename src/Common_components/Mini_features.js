37e8520606cd99dacc010203ba3e4685ceb91202
MarlonAlf <96500502+MarlonAlf@users.noreply.github.com>
2024-06-28 15:07:42 +0200

    Add files via upload

--- src/Common_components/Mini_features.js
+++ src/Common_components/Mini_features.js
@@ -2,28 +2,28 @@ const Mini_feature = () => {
     return ( 
         <section id="feature">
         <div className="ft-box">
-            <img src="images/decoration/features-1.png"/>
+            <img src={`${process.env.PUBLIC_URL}/images/decoration/features-1.png`}/>
             <h6>Free Shipping</h6>
         </div>
         <div className="ft-box">
-            <img src="images/decoration/features-2.png"/>
+             <img src={`${process.env.PUBLIC_URL}/images/decoration/features-2.png`}/>
             <h6>Free Shipping</h6>
         </div>
         <div className="ft-box">
-            <img src="images/decoration/features-3.png"/>
+            <img src={`${process.env.PUBLIC_URL}/images/decoration/features-3.png`}/>
             <h6>Free Shipping</h6>
         </div>
         <div className="ft-box">
-            <img src="images/decoration/features-4.png"/>
+            <img src={`${process.env.PUBLIC_URL}/images/decoration/features-4.png`}/>
             <h6>Free Shipping</h6>
         </div>
         <div className="ft-box">
-            <img src="images/decoration/features-5.png"/>
+            <img src={`${process.env.PUBLIC_URL}/images/decoration/features-5.png`}/>
             <h6>Free Shipping</h6>
         </div>
         <div className="ft-box">
-            <img src="images/decoration/features-6.png"/>
-            <h6>Free Shipping</h6>
+            <img src={`${process.env.PUBLIC_URL}/images/decoration/features-6.png`}/>
+             <h6>Free Shipping</h6>
         </div>
     </section>
      );
